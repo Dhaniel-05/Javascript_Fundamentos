7e7122be482371e1edf220e32191d6cfeb313bb0
Dhaniel-05 <dhanymarth@gmail.com>
2025-01-22 16:00:30 -0500

    Teoría Bases Datos

---
+++ Bases_Datos/ex_74_Bases_Datos_Teoría/ex_74_bases_datos_teoria.js
@@ -0,0 +1,14 @@
+/* Bases de Datos
+
+Una base de datos es un conjunto de datos organizados y estructurados en un sistema informático, pertenecientes a un mismo contexto. En términos simples, las bases de datos permiten almacenar grandes cantidades de información que debe estar relacionada entre sí, formando una unidad lógica. 
+
+Para comprender este concepto, puede hacerse una analogía con una biblioteca. Una biblioteca puede considerarse una base de datos compuesta principalmente por libros, los cuales están organizados en un índice. Es decir, están indexados o relacionados mediante algún tipo de código o clave, lo que facilita una organización eficiente de toda esa información.
+
+En el ámbito del desarrollo de aplicaciones web, las bases de datos son fundamentales porque proporcionan una forma eficiente y escalable de almacenar y recuperar datos. Estas permiten acceder y manipular grandes volúmenes de información de manera rápida y sencilla. Además, no se limitan únicamente a estas funciones, ya que también ofrecen mecanismos de seguridad y privacidad para garantizar el acceso y almacenamiento seguro de los datos.
+
+Cabe destacar que guardar información en una base de datos no implica simplemente almacenar datos de manera desordenada, como quien arroja objetos dentro de un cajón. La clave radica en la estructura y la organización con que se guardan los datos. En este sentido, existen dos enfoques principales en el diseño de bases de datos: **las bases de datos relacionales** y **las bases de datos no relacionales**. 
+
+La principal diferencia entre estos enfoques radica en la manera en que los datos se almacenan y se relacionan. Las bases de datos relacionales están diseñadas para estructurar información en tablas relacionadas mediante claves primarias y foráneas, priorizando la consistencia y la integridad de los datos. Por otro lado, las bases de datos no relacionales son más flexibles y se adaptan mejor a datos que no poseen una estructura definida o que varían constantemente, como los utilizados en aplicaciones modernas que manejan grandes cantidades de datos heterogéneos.
+
+Por lo tanto cuando se desarrolla un aplicativo, el diseño y el enfoque elegido para una base de datos dependerán de factores como la cantidad de información a manejar, el objetivo de la aplicación conectada a la base y las características de los datos almacenados, ya sean estáticos y estructurados o dinámicos y no estructurados.
+ */
